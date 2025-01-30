b5516bb8f14c08dffa9a003bd0753dfc390c3410
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2025-01-30 22:05:57 +0700

    fix: button click tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,26 @@
 "use client";
 
-import { useState } from "react";
-import { useGA } from "@/hooks/useGA";
+import { sendGAEvent } from "@next/third-parties/google";
+
+// import { useState } from "react";
+// import { useGA } from "@/hooks/useGA";
 
 export default function Home() {
-  const { action } = useGA();
-  const [clicked, setClicked] = useState(false);
+//   const { action } = useGA();
+//   const [clicked, setClicked] = useState(false);
 
-  const handleButtonClick = () => {
-    setClicked(true);
-    action("button_click", "My Button Clicked");
-  };
+//   const handleButtonClick = () => {
+//     setClicked(true);
+//     action("button_click", "My Button Clicked");
+//   };
 
   return (
     <div>
       <h1>This is the main page</h1>
-      <button
-        onClick={handleButtonClick}
-        className={`px-4 py-2 text-white rounded ${
-          clicked ? "bg-green-500" : "bg-blue-500"
-        }`}
+	  <button
+        onClick={() => sendGAEvent('event', 'buttonClicked', { value: 'xyz' })}
       >
-        {clicked ? "Clicked!" : "Click Me"}
+        Send Event
       </button>
     </div>
   );
