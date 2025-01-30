ccbf1a17c286d56894e5fb9ff93686840cd2b31e
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2025-01-30 21:29:23 +0700

    feat: button click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { useGA } from "@/hooks/useGA"
+
+
 export default function Home() {
-	return <h1>This is the main page</h1>
+  const { action } = useGA()
+
+  const handleButtonClick = () => {
+    action('button_click', 'My Button Clicked')
+  }
+
+  return (
+    <div>
+      <h1>This is the main page</h1>
+      <button onClick={handleButtonClick}>Click Me</button>
+    </div>
+  )
 }
