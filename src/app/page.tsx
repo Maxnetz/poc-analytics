a996348986fae424b7e52742456c8e83d4d93fbf
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2025-01-30 22:26:50 +0700

    chore: change tracking click func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client'
 
-import { sendGAEvent } from "@next/third-parties/google";
+import { Analytic } from '@/hooks/useGA'
+import { sendGAEvent } from '@next/third-parties/google'
 
-// import { useState } from "react";
-// import { useGA } from "@/hooks/useGA";
+const buttonClick = <T extends keyof Analytic>(action: T, value: Analytic[T]) => {
+	sendGAEvent({
+		eventName: action,
+		value,
+	})
+}
 
 export default function Home() {
-//   const { action } = useGA();
-//   const [clicked, setClicked] = useState(false);
+	return (
+		<div>
+			<h1>This is the main page</h1>
 
-//   const handleButtonClick = () => {
-//     setClicked(true);
-//     action("button_click", "My Button Clicked");
-//   };
+			<button onClick={() => buttonClick('button_click', 'session_01_button')}>session 01 detail</button>
 
-  return (
-    <div>
-      <h1>This is the main page</h1>
-	  <button
-        onClick={() => sendGAEvent('event', 'buttonClicked', { value: 'xyz' })}
-      >
-        Send Event
-      </button>
-    </div>
-  );
+			{/* <button onClick={() => buttonClick('link_click', 'connext')}>Banner</button> */}
+
+			<button onClick={() => buttonClick('banner_click', 'session_banner_01')}>Session 01 Banner</button>
+
+			<button onClick={() => buttonClick('form_submit', true)}>register session</button>
+		</div>
+	)
 }
--- src/hooks/useGA.ts
+++ src/hooks/useGA.ts
@@ -1,11 +1,12 @@
 import { sendGAEvent } from '@next/third-parties/google'
 
 // The analyic each type will be updated again here is the workaround to support many actions
-interface Analytic {
-	button_click: string
-	link_click: number
-	form_submit: boolean
-}
+export interface Analytic {
+	button_click: string;
+	link_click: string;
+	form_submit: boolean;
+	banner_click: string;
+  }
 
 export const useGA = () => {
 	const pageView = (pathname: string) => {
@@ -15,15 +16,15 @@ export const useGA = () => {
 		})
 	}
 
-	const action = <T extends keyof Analytic>(action: T, value: Analytic[T]) => {
+	const buttonClick = <T extends keyof Analytic>(action: T, value: Analytic[T]) => {
 		sendGAEvent({
-			eventName: action,
-			value,
-		})
-	}
+		  eventName: action,
+		  value,
+		});
+	  };
 
 	return {
 		pageView,
-		action,
+		action: buttonClick,
 	}
 }
