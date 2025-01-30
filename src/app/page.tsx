7367e633b6530568a6056524f25bf23ad429dfd7
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2025-01-30 22:28:52 +0700

    fix: display flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,10 @@ const buttonClick = <T extends keyof Analytic>(action: T, value: Analytic[T]) =>
 
 export default function Home() {
 	return (
-		<div>
+		<div className='flex flex-col'>
 			<h1>This is the main page</h1>
 
 			<button onClick={() => buttonClick('button_click', 'session_01_button')}>session 01 detail</button>
-
-			{/* <button onClick={() => buttonClick('link_click', 'connext')}>Banner</button> */}
 
 			<button onClick={() => buttonClick('banner_click', 'session_banner_01')}>Session 01 Banner</button>
 
