a8a192051b7be9eca24b2cc16c427e28cf1b24f4
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2025-01-30 22:51:14 +0700

    fix: click func

--- src/hooks/useGA.ts
+++ src/hooks/useGA.ts
@@ -2,11 +2,11 @@ import { sendGAEvent } from '@next/third-parties/google'
 
 // The analyic each type will be updated again here is the workaround to support many actions
 export interface Analytic {
-	button_click: string;
-	link_click: string;
-	form_submit: boolean;
-	banner_click: string;
-  }
+	button_click: string
+	link_click: string
+	form_submit: boolean
+	banner_click: string
+}
 
 export const useGA = () => {
 	const pageView = (pathname: string) => {
@@ -17,15 +17,12 @@ export const useGA = () => {
 	}
 
 	const buttonClick = <T extends keyof Analytic>(action: T, value: Analytic[T]) => {
-		console.log('GA Event:', action, value);  // Debug log to see if this is called
-		sendGAEvent({
-		  eventName: action,
-		  value,
-		});
-	  };
+		console.log('GA Event:', action, value) // Debug log to see if this is called
+		sendGAEvent('event', action, { value })
+	}
 
-	  return {
+	return {
 		pageView,
-		buttonClick, 
-	  };
+		buttonClick,
+	}
 }
