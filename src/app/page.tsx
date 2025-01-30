668f671738fbb53864f6e5c22d7af15eb21ea905
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2025-01-30 21:34:00 +0700

    fix: use client click action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { useGA } from "@/hooks/useGA"
+"use client";
 
+import { useGA } from "@/hooks/useGA";
 
 export default function Home() {
-  const { action } = useGA()
+  const { action } = useGA();
 
   const handleButtonClick = () => {
-    action('button_click', 'My Button Clicked')
-  }
+    action("button_click", "My Button Clicked");
+  };
 
   return (
     <div>
       <h1>This is the main page</h1>
       <button onClick={handleButtonClick}>Click Me</button>
     </div>
-  )
+  );
 }
